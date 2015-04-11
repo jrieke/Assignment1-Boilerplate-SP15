22e0d656bbf3b4eccbd90da62eb54acaa70c1215
Johannes Rieke <johannes.rieke@gmail.com>
2015-04-11 01:46:11 -0700

    test

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var models = require('./models');
 
 //client id and client secret here, taken from .env
 dotenv.load();
-console.log(process.env);
 var INSTAGRAM_CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
+console.log(INSTAGRAM_CLIENT_ID);
 var INSTAGRAM_CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
 var INSTAGRAM_CALLBACK_URL = process.env.INSTAGRAM_CALLBACK_URL;
 var INSTAGRAM_ACCESS_TOKEN = "";
