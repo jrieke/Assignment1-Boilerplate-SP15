b5d5442aa868dd6e5666036246f64abdfc8b717f
Johannes Rieke <johannes.rieke@gmail.com>
2015-04-11 01:52:36 -0700

    test

--- app.js
+++ app.js
@@ -20,15 +20,15 @@ var models = require('./models');
 dotenv.load();
 var INSTAGRAM_CLIENT_ID = process.env.instagram_client_id;
 console.log(INSTAGRAM_CLIENT_ID);
-var INSTAGRAM_CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
-var INSTAGRAM_CALLBACK_URL = process.env.INSTAGRAM_CALLBACK_URL;
+var INSTAGRAM_CLIENT_SECRET = process.env.instagram_client_secret;
+var INSTAGRAM_CALLBACK_URL = process.env.instagram_callback_url;
 var INSTAGRAM_ACCESS_TOKEN = "";
 Instagram.set('client_id', INSTAGRAM_CLIENT_ID);
 Instagram.set('client_secret', INSTAGRAM_CLIENT_SECRET);
 
 
 //connect to database
-mongoose.connect(process.env.MONGODB_CONNECTION_URL);
+mongoose.connect(process.env.mongodb_connection_url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
